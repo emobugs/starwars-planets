37ce7d69b0cc608ce2e7b6581c9ef0170e5de847
Emo <emotupev11@gmail.com>
2021-09-15 23:57:23 +0300

    fetch data from api

--- src/app/Application.js
+++ src/app/Application.js
@@ -1,8 +1,8 @@
-import config from '../config';
-import EventEmitter from 'eventemitter3';
+import config from "../config";
+import EventEmitter from "eventemitter3";
 
 const EVENTS = {
-  APP_READY: 'app_ready',
+	APP_READY: "app_ready",
 };
 
 /**
@@ -10,29 +10,44 @@ const EVENTS = {
  * All configurations are described in src/config.js
  */
 export default class Application extends EventEmitter {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.config = config;
-    this.data = {};
+		this.config = config;
+		this.data = {};
 
-    this.init();
-  }
+		this.init();
+	}
 
-  static get events() {
-    return EVENTS;
-  }
+	static get events() {
+		return EVENTS;
+	}
 
-  /**
-   * Initializes the app.
-   * Called when the DOM has loaded. You can initiate your custom classes here
-   * and manipulate the DOM tree. Task data should be assigned to Application.data.
-   * The APP_READY event should be emitted at the end of this method.
-   */
-  async init() {
-    // Initiate classes and wait for async operations here.
+	/**
+	 * Initializes the app.
+	 * Called when the DOM has loaded. You can initiate your custom classes here
+	 * and manipulate the DOM tree. Task data should be assigned to Application.data.
+	 * The APP_READY event should be emitted at the end of this method.
+	 */
+	async init() {
+		// Initiate classes and wait for async operations here.
+		const fetchGeneral = await fetch("https://swapi.boom.dev/api/planets");
+		const dataGeneral = await fetchGeneral.json();
+		let next = dataGeneral.next;
 
-    this.emit(Application.events.APP_READY);
-  }
+		let count = 1;
+
+		while (next !== null) {
+			const fetchData = await fetch(`https://swapi.boom.dev/api/planets?page=${count}`);
+			// eslint-disable-next-line newline-after-var
+			const data = await fetchData.json();
+			next = data.next;
+			count++;
+			this.data.planets = [...this.data.planets, ...data.results];
+		}
+
+		// set the count for the planets count
+		this.data.count = dataGeneral.count;
+		this.emit(Application.events.APP_READY);
+	}
 }
-
